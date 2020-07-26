c1d6c98cc5156c3f2fb8ce05b6972dcbf6b59d01
DOGUK <dgkim1994@gmail.com>
2020-07-26 19:12:08 +0900

    Add:LoginFormLayout

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,15 +1,59 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Button from 'components/Button';
-import * as Text from 'components/Text';
-import theme from 'layout/theme';
+import Link from "next/link";
+import styled from "styled-components";
 
-import Login from 'containers/Login';
-
-export default function LoginPage() {
+const Login = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <Container>
+      <Form>
+        <Input
+          type="email"
+          autoComplete="email"
+          placeholder="email@admin.com"
+          // onChange={onChange}
+          // value={email}
+        />
+        <Input
+          type="password"
+          autoComplete="new-password"
+          placeholder="password"
+          // onChange={onChange}
+          // value={email}
+        />
+        <BtnContainer>
+          <Button>로그인</Button>
+          <Link href="/register">
+            <a>회원가입</a>
+          </Link>
+        </BtnContainer>
+      </Form>
+    </Container>
   );
-}
+};
+
+export default Login;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  flex-direction: column;
+`;
+
+const BtnContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Form = styled.form`
+  border: 1px solid black;
+  width: 768px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
