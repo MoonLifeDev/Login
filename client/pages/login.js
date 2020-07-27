049709812270cbdd88372335b76796d6467fee54
DOGUK <dgkim1994@gmail.com>
2020-07-27 17:09:22 +0900

    Update:loginDesignFixForm

--- client/pages/login.js
+++ client/pages/login.js
@@ -5,25 +5,35 @@ const Login = () => {
   return (
     <Container>
       <Form>
-        <Input
-          type="email"
-          autoComplete="email"
-          placeholder="email@admin.com"
+        <LoginItem>
+          <Input
+            type="email"
+            autoComplete="email"
+            placeholder="email@admin.com"
           // onChange={onChange}
           // value={email}
-        />
-        <Input
-          type="password"
-          autoComplete="new-password"
-          placeholder="password"
+          />
+          <Input
+            type="password"
+            autoComplete="new-password"
+            placeholder="password"
           // onChange={onChange}
           // value={email}
-        />
-        <BtnContainer>
-          <Button>로그인</Button>
+          />
+        </LoginItem>
+        <LinkList>
           <Link href="/register">
-            <a>회원가입</a>
+            <LinkAnchor register>회원가입</LinkAnchor>
           </Link>
+          <Link href="/#">
+            <LinkAnchor>아이디 / 비밀번호 찾기</LinkAnchor>
+          </Link>
+        </LinkList>
+        <BtnContainer>
+          <Button login color>
+            로그인
+          </Button>
+          <Button lastChlid>취소</Button>
         </BtnContainer>
       </Form>
     </Container>
@@ -31,29 +41,73 @@ const Login = () => {
 };
 
 export default Login;
-
+// styled-components
 const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
   height: 100vh;
-  flex-direction: column;
-`;
-
-const BtnContainer = styled.div`
-  display: flex;
-  align-items: center;
 `;
 const Form = styled.form`
-  border: 1px solid black;
-  width: 768px;
-  height: 300px;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 368px;
+  padding: 28px;
+  border-radius: 15px;
+  box-shadow: 0 2px 9px 0 rgba(0, 0, 0, 0.1), 0 3px 6px 0 rgba(0, 0, 0, 0.06);
+`;
+const LoginItem = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
 `;
+const Input = styled.input`
+  margin: 0 0 16px 0;
+  padding: 16px 8px;
+  font-size: 16px;
+  border-radius: 0.375em;
+  border: 1px solid rgba(210, 214, 220, 1);
+  transition: all 0.25s cubic-bezier(0.4, 0, 0.2, 1);
+  &:placeholder {
+    color: rgba(107, 114, 128, 1);
+  }
+  &:focus {
+    border-color: rgba(28, 100, 242, 0.8);
+    box-shadow: 0 6px 10px -10px rgba(28, 100, 242, 0.8);
+  }
+`;
+const LinkList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 16px 0 10px 0;
+`;
+const LinkAnchor = styled.a`
+  color: ${props => props.register ? "rgba(28,100,242,1)" : "#1d1d1f"};
+  border-bottom: ${props => props.register ? "1px solid rgba(28,100,242)" : "0"};
+  position: relative;
+  font-size: 15px;
+  text-decoration: none;
+  transition: all 0.25s cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    color: ${props => props.register ? "rgba(28,100,242,.6)" : "rgba(107, 114, 128, 1)"};
+    border-bottom: ${props => props.register ? "1px solid rgba(28,100,242,.6)" : "0"};
+  }
+`;
+const BtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Button = styled.button`
+  margin: ${props => (props.lastChlid ? "0" : "0 0 16px 0")};
+  background-color: ${props => (props.login ? "rgba(28,100,242)" : "#ecf0f1")};
+  color: ${props => (props.color ? "#fff" : "rgba(107,114,128,1)")};
+  padding: 10px 0;
+  font-size: 15px;
+  border: 0;
+  border-radius: 0.375em;
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    background-color: ${props =>
+    props.login ? "rgba(28,100,242,.8)" : "#ecf0f1"};
+  }
+`;
 
-const Input = styled.input``;
-
-const Button = styled.button``;
